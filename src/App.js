7f4b3f3f9b69a9ea48c9d96185040681ee90ac0e
Hemanth Kumar <avisihks@gmail.com>
2024-05-31 14:41:27 +0530

    Add 3 D Modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment, ContactShadows } from "@react-three/drei";
+import { Model } from "./Table";
 
-function App() {
-  return <div className="App">Hii</div>;
+export default function App() {
+  return (
+    <>
+      <Canvas shadows camera={{ position: [0, 0, 1.66] }}>
+        <Environment preset="forest" />
+        <Model />
+        {/* <ContactShadows position={[0, -0.8, 0]} color="#ffffff" /> */}
+        <OrbitControls position={[0, 0, 0]} />
+      </Canvas>
+    </>
+  );
 }
-
-export default App;
