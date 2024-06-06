2397cc0d256eb80762da18f9edd6ecf6f410c427
Hemanth Kumar <avisihks@gmail.com>
2024-06-06 15:05:50 +0530

    Add Slow motion to table drawer

--- src/NewDrawer.jsx
+++ src/NewDrawer.jsx
@@ -1,7 +1,7 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { useGLTF } from "@react-three/drei";
+import { a, useSpring } from "@react-spring/three";
 import Dresser2 from "./models/dresser2.glb";
-import Drawer from "./Drawer";
 
 function TableDrawer(props) {
   const { nodes, materials } = useGLTF(Dresser2);
@@ -12,43 +12,49 @@ function TableDrawer(props) {
   const positionX = -0.35 * (1 - scale[0]);
   const positionZ = -0.3 * (1 - scale[1]);
 
+  const { position } = useSpring({
+    position: open
+      ? [0.7 + positionX, 0.26 + positionZ, 0]
+      : [0.35 + positionX, 0.26 + positionZ, 0],
+    config: { tension: 50, friction: 50 },
+  });
+
+  const drawerPosition =
+    props?.count === 1
+      ? props.i
+      : props?.count === 2
+      ? props?.i === 0
+        ? -0.3
+        : 0.3
+      : props?.count === 3
+      ? props?.i === 0
+        ? -0.6
+        : props?.i === 1
+        ? 0
+        : 0.6
+      : props?.count === 4
+      ? props?.i === 0
+        ? -0.9
+        : props?.i === 1
+        ? -0.3
+        : props?.i === 2
+        ? 0.3
+        : 0.9
+      : props?.count === 5
+      ? props?.i === 0
+        ? -1.2
+        : props?.i === 1
+        ? -0.6
+        : props?.i === 2
+        ? 0
+        : props?.i === 3
+        ? 0.6
+        : 1.2
+      : 0;
+
   return (
-    <group
-      position={[
-        open ? 0.7 + positionX : 0.35 + positionX,
-        0.26 + positionZ,
-        props?.count === 1
-          ? props.i
-          : props?.count === 2
-          ? props?.i === 0
-            ? -0.3
-            : 0.3
-          : props?.count === 3
-          ? props?.i === 0
-            ? -0.6
-            : props?.i === 1
-            ? 0
-            : 0.6
-          : props?.count === 4
-          ? props?.i === 0
-            ? -0.9
-            : props?.i === 1
-            ? -0.3
-            : props?.i === 2
-            ? 0.3
-            : 0.9
-          : props?.count === 5
-          ? props?.i === 0
-            ? -1.2
-            : props?.i === 1
-            ? -0.6
-            : props?.i === 2
-            ? 0
-            : props?.i === 3
-            ? 0.6
-            : 1.2
-          : 0,
-      ]}
+    <a.group
+      position={position.to((x, y, z) => [x, y, drawerPosition])}
       rotation={[1.58, 0, 0]}
       scale={[0.0035, 0.0035, 0.0035]}
     >
@@ -59,7 +65,7 @@ function TableDrawer(props) {
         onClick={() => setOpen(!open)}
         scale={[1, 1, 1]}
       />
-    </group>
+    </a.group>
   );
 }
 
